39b78d68f36a462a8fe37c121ae003f1d0a51886
FFanwaregithub <41316356+FFanwaregithub@users.noreply.github.com>
2018-07-17 12:53:54 +0800

    Create automateGPU.sh

---
+++ automateGPU.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#################################################
+automateGPU.sh  ver:0.01   Copyright 2018
+=================================================
+Author: Fan[fwdarling@gmail.com]
+Homepage:http://github.com/FFanwaregithub/
+=================================================
+Usage:
+      chmod +x ./aumateGPU.sh
+      sudo ./automateGPU.sh
+      sudo ./automateGPU.sh -a
+#################################################
